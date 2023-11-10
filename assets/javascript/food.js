e1154c2ac6e9c77b5fa476e37bfc9efbdb6fae91
torfirst <victoriah.first@gmail.com>
2023-11-09 20:23:43 -0600

    updated food.js file

--- assets/javascript/food.js
+++ assets/javascript/food.js
@@ -2,17 +2,45 @@
 var ingredientsEl = document.querySelector("#ingredient")
 var searchBtnEl = document.querySelector("#searchBtn")
 var foodApiKey = "ea483b863cdd433eb55d9222c3cc0c4d";
+var recipeResults = [];
+var displayResults = document.getElementById("results");
 
 function callAPI(event) {
+    recipeResults = []; // resets the results
+    while (displayResults.firstChild) {
+        displayResults.removeChild(displayResults.firstChild);
+    } // clears result divs
     event.preventDefault()
-    var requestURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=ea483b863cdd433eb55d9222c3cc0c4d&includeIngredients=" + ingredientsEl.value
-    
+    var requestURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${foodApiKey}&ingredients=${ingredientsEl.value}&number=10&ignorePantry=false`;
+
+    console.log(ingredientsEl.value);
+
     fetch(requestURL)
     .then(function(response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);    
+        recipeResults = data;
+        if (recipeResults.length > 0){
+            recipeResults.forEach(recipe => {
+                var recipeDiv = document.createElement("div");
+                recipeDiv.innerHTML = recipe?.title;
+               
+                var recipeCard = `https://api.spoonacular.com/recipes/${recipe.id}/card?apiKey=${foodApiKey}`;
+                fetch (recipeCard)
+                .then(function(recipeCardResponse) {
+                    return recipeCardResponse.json();
+                })
+                .then(function(res){
+                    console.log(res);
+                    var displayRecipe = document.createElement("img");
+                    displayRecipe.src = res.url;
+                    recipeDiv.appendChild(displayRecipe); 
+                    displayResults.appendChild(recipeDiv);
+                })
+            })
+        }
     })
 }
 
