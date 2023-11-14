2be2b8776e47480f99a617bd23a3118a770bf39b
torfirst <victoriah.first@gmail.com>
2023-11-13 18:19:50 -0600

    Merged index.html styling

--- assets/javascript/drink.js
+++ assets/javascript/drink.js
@@ -6,34 +6,6 @@ var displayResults = document.getElementById("results")
 function callDrinkAPI(event) {
     var requestURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinksEl.value}`;
 
-    if (inputs[i].checked = !inputs[i].checked) {
-        var drinkSearch = document.querySelector(".drinkSearch");
-        drinkSearch.removeAttribute(".hide");
-    }
-    
-    drinkResults = []; // resets the results
-    while (displayResults.firstChild) {
-        displayResults.removeChild(displayResults.firstChild);
-    } // clears result divs
-    event.preventDefault()
-
-    console.log(drinksEl.value);
-
-    fetch(requestURL)
-        .then(function (response) { // made API call, awaiting data response
-            return response.json();
-        })
-        .then(function (data) { // API call complete, generate divs to display data response
-            console.log(data);
-            drinkResults = data;
-            if (drinkResults.length > 0) {
-                drinkResults.forEach(drink => {
-                    var drinkDiv = document.createElement("div");
-                    drinkDiv.innerHTML = drink.title;
-                    displayResults.appendChild(drinkDiv);
-                })
-            }
-        })
 }
 
 searchBtnEl.addEventListener("click", callDrinkAPI);
